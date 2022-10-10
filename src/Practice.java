cda1aab2f39fdf0bf8a33d73feb7103af3b3e8ed
Rob <robertace.gonzales@gmail.com>
2022-10-10 14:20:03 -0700

    print statement

--- src/Practice.java
+++ src/Practice.java
@@ -2,5 +2,6 @@ public class Practice {
     public static void main(String[] args) throws Exception {
         TrueBanana BN = new TrueBanana();
         BN.print();
+        System.out.println("Make it make sense");
     }
 }
